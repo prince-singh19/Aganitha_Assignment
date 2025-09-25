2ea02e58041310adb1cd30e493d81ea803a0ec92
prince-singh19 <princesingh9621680755@gmail.com>
2025-09-25 20:05:05 +0530

    Add all file firstly

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline ,color-blue-500'>
+      return princedd
+    </div>
+  )
+}
+
+export default App
